8e0cbdc1d2ca9e6dd3f48354a851de158e28b60f
daniloqb <daniloqb@gmail.com>
2016-10-19 22:27:47 -0200

    split_url para separar a uri e os parametros

--- C-scripts/main.cpp
+++ C-scripts/main.cpp
@@ -60,12 +60,93 @@ void test_checkTag() {
     }
     
 }
+const int MAX_PARAMS = 3;
+
+
+typedef struct param {
+    char name[20];
+    char value[20];
+    
+}Params;
+
+char url[255] = "/rgblamp?color=255;brightness=100";
+// char url[] = "/rgblamp?red=255;green=0;blue=255";
+Params params[MAX_PARAMS];
+char uri[255];
+
+
+
+
+void split_url(char  * url, char * uri, Params *params){
+
+  
+    char * raw_params[MAX_PARAMS];
+    char * token;
+    
+    int num_params = 0;
+    
+    token = strtok(url,"?");
+    strncpy(uri,token,255);
+    token = strtok(NULL,"?");
+
+    
+    token = strtok(token,"&;");
+    
+    while(token != NULL && num_params < MAX_PARAMS){
+        raw_params[num_params] = token;
+        token = strtok(NULL,"&;");
+        num_params++;
+    }
+    
+    for(int i=0; i < num_params;i++){
+    
+      token = strtok(raw_params[i],"=");
+      strncpy(params[i].name,token,20);
+      token = strtok(NULL,"=");
+      if(token !=NULL){
+
+        strncpy(params[i].value,token,20);
+      }else{
+        strncpy(params[i].value,"",20);
+
+      }
+      
+    }
+   
+}
+
+
+char * getParam(char * name){
+    
+       
+    for(int i=0; i < MAX_PARAMS;i++){
+        
+        if (strcmp(params[i].name,name) == 0){
+            return params[i].value;
+        }
+    }
+    
+    return NULL;
+}
+
+
 
 int main(int argc, char** argv) {
 
-    test_checkTag();
+   // test_checkTag();
 
-
+    
+     strcpy(uri,"");
+    for(int i=0;i< MAX_PARAMS;i++){
+        strcpy(params[i].name,"");
+        strcpy(params[i].value,"");
+    }
+    
+    split_url(url,uri,params);
+    
+    
+    printf("Param Value: %s", getParam("brightness"));
+    
     return 0;
 }
 
